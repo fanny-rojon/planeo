8169b159c2627b3cdbf7e9528b5148c4ae05095b
bitna kim <bitna153@gmail.com>
2019-11-18 14:11:18 +0100

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,12 +31,15 @@ user4 = User.create!(
 
 group1 = Group.create!(
   name: "Le wagon",
+  photo: "http://www.w3.org/1999/xlink"
   )
 
 group2 = Group.create!(
   name: "Sisters",
+  photo: "https://pmcdeadline2.files.wordpress.com/2016/12/stepsisters.png?w=595&h=383&crop=1"
   )
 
 group3 = Group.create!(
   name: "Girls Night",
+  photo: "https://www.thekingsferry.co.uk/sites/default/files/xmas-party-50-50%20%281%29.jpg"
   )
