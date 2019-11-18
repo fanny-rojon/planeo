3c85f938f58bfb49a5796ffa09531cd02704eaad
Fanny Rojon <fanny.rojon22@gmail.com>
2019-11-18 18:03:53 +0100

    started working on events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,18 @@
 class EventsController < ApplicationController
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    @group = Group.find(params[:group_id])
+    @event.group = @group
+    @event.organizer = current_user
+    if @event.save!
+      redirect_to group_event_path(@event)
+    else
+      render :new
+    end
   end
 
   def show
@@ -15,5 +25,11 @@ class EventsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :state, :address, :photo)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_134757) do
+ActiveRecord::Schema.define(version: 2019_11_18_164519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_11_18_134757) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "group_id"
+    t.string "organizer"
     t.index ["group_id"], name: "index_events_on_group_id"
   end
 
