39c0b6e5d0973b74add9a254ee6d5c7612c58b8e
Karim Soussi <mrksoussi@gmail.com>
2019-11-18 17:26:34 +0100

    Did some work on the groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,20 +5,40 @@ class GroupsController < ApplicationController
   end
 
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.new(group_params)
+    @group.user_ids = current_user.id
+    if @group.save!
+      redirect_to groups_path
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to groups_path
   end
 
   def destroy
+    Group.destroy(params[:id])
+    redirect_to groups_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :photo)
   end
 end
