8d01b2e0450b76f3427370edaeecd611b1c6463a
felipe de alba <felipe.de.alba.alegre@gmail.com>
2019-11-19 15:03:32 +0100

    group index done

--- db/seeds/group_seeds.rb
+++ db/seeds/group_seeds.rb
@@ -1,7 +1,7 @@
-puts "Destroying groups"
+puts "Destroying groups..."
 Group.destroy_all
 
-puts "Creating groups"
+puts "Creating groups..."
 
 
 Group.create!(
@@ -10,12 +10,12 @@ Group.create!(
 )
 
 Group.create!(
-  name: "Sisters",
+  name: "Sisters...",
   photo: "https://pmcdeadline2.files.wordpress.com/2016/12/stepsisters.png?w=595&h=383&crop=1"
 )
 
 Group.create!(
-  name: "Girls Night",
+  name: "Girls Night...",
   photo: "https://www.thekingsferry.co.uk/sites/default/files/xmas-party-50-50%20%281%29.jpg"
 )
-puts "Groups created"
+puts "Groups created."
