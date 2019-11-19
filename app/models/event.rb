882f3788acec5378a6eb13b434a07bd1715cba03
Fanny Rojon <fanny.rojon22@gmail.com>
2019-11-19 16:04:45 +0100

    working on show page for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :group
+  has_many :usergroups, through: :group
+  has_one :user, through: :group
   validates :name, presence: true
   validates :state, inclusion: { in: ["proposed", "organized", "confirmed", "past"] }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,12 @@ require_relative './seeds/user_seeds'
 require_relative './seeds/group_seeds'
 require_relative './seeds/event_seeds'
 
+puts "Assiging users to groups..."
 User.all.each do |us|
   Group.first.users << us
 end
 
+
+Event.find(1).organizer = user1
+Event.find(2).organizer = user2
+Event.find(3).organizer = user3
