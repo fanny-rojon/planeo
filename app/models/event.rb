7da953a908cbb1a16563bb083993d2ac15b302e8
Karim Soussi <mrksoussi@gmail.com>
2019-11-19 17:18:42 +0100

    Added link to groups

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
+  geocoded_by :address
   mount_uploader :photo, PhotoUploader
   belongs_to :group
   validates :name, presence: true
   validates :state, inclusion: { in: ["proposed", "organized", "confirmed", "past"] }
+  after_validation :geocode, if: :will_save_change_to_address?
 end
