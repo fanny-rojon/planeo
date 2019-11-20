4feb54699abd92218cc3e0c5e895be15c3c0acfb
Fanny Rojon <fanny.rojon22@gmail.com>
2019-11-20 17:26:27 +0100

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@ group1 = Group.find_by(name: "Le Wagon")
 group2 = Group.find_by(name: "Sisters")
 group3 = Group.find_by(name: "Girls Night")
 group4 = Group.find_by(name: "Call of Duty")
+group5 = Group.find_by(name: "Female Coders")
 
 # Le Wagon Users
 fanny = User.find_by(name: "Fanny")
@@ -44,6 +45,8 @@ group2.users << [caroline, laure, delphine, celine, fanny]
 group3.users << [beatriz, isa, violetta, bea, anita, fanny]
 
 group4.users << [karim, felipe, niklas, herve, javier]
+
+group5.users << [bitna, fanny, carol, ana, gabriela, belen, maja]
 
 puts "Seeds done!!!"
 
