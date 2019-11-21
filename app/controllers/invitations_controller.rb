bbca67a4477d417e3d2b166f19234569b90a74ca
Fanny Rojon <fanny.rojon22@gmail.com>
2019-11-21 12:36:12 +0100

    invite users to groups backend done

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,4 +1,6 @@
 class InvitationsController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def register
     redirect_to new_user_registration_path(code: params[:code])
   end
