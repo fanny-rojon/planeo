a68f6cd8bd4add162ea0e1ce99c61f4697804825
felipe de alba <felipe.de.alba.alegre@gmail.com>
2019-11-21 16:41:34 +0100

    done some validations and group index edit

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
   belongs_to :group
   has_many :usergroups, through: :group
   belongs_to :organizer, class_name: "User"
-  validates :name, presence: true
+  validates :name, length: { in: 4..24 }
   validates :state, inclusion: { in: ["proposed", "organized", "confirmed", "past"] }
   after_validation :geocode, if: :will_save_change_to_address?
   after_update :geocode
